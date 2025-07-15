ac3bcec904f27a174c477a0e00b62f26adc1684d
Butyrin Ivan <vanya.1.butyrin@gmail.com>
2025-07-15 20:44:51 +0300

    models

---
+++ internal/models/models.go
@@ -0,0 +1,33 @@
+package models
+
+import "time"
+
+type Post struct {
+	ID            string    `json:"id"`
+	Title         string    `json:"title"`
+	Content       string    `json:"content"`
+	AuthorID      string    `json:"authorId"`
+	AllowComments bool      `json:"allowComments"`
+	CreatedAt     time.Time `json:"createdAt"`
+}
+
+type Comment struct {
+	ID        string    `json:"id"`
+	PostID    string    `json:"postId"`
+	ParentID  *string   `json:"parentId"`
+	AuthorID  string    `json:"authorId"`
+	Content   string    `json:"content"`
+	CreatedAt time.Time `json:"createdAt"`
+}
+
+type PaginatedComments struct {
+	Comments   []Comment `json:"comments"`
+	TotalCount int       `json:"totalCount"`
+	NextCursor *string   `json:"nextCursor"`
+}
+
+type PaginatedPosts struct {
+	Posts      []Post  `json:"posts"`
+	TotalCount int     `json:"totalCount"`
+	NextCursor *string `json:"nextCursor"`
+}
