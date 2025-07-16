c6952ebf8f2f9aa69e13d706e6092df37785384a
Butyrin Ivan <vanya.1.butyrin@gmail.com>
2025-07-16 22:50:33 +0300

    main.go and go.mod

---
+++ cmd/server/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"flag"
+	"log"
+
+	"github.com/ButyrinIA/system/internal/config"
+	"github.com/ButyrinIA/system/internal/server"
+	"github.com/ButyrinIA/system/internal/storage"
+	"github.com/ButyrinIA/system/internal/storage/memory"
+	"github.com/ButyrinIA/system/internal/storage/postgres"
+)
+
+func main() {
+	configPath := flag.String("config", "config.yaml", "путь к файлу конфигурации")
+	storageType := flag.String("storage", "memory", "тип хранилища: memory или postgres")
+	flag.Parse()
+
+	cfg, err := config.Load(*configPath)
+	if err != nil {
+		log.Fatalf("Не удалось загрузить конфигурацию: %v", err)
+	}
+
+	var store storage.Storage
+	switch *storageType {
+	case "postgres":
+		store, err = postgres.New(cfg.Postgres.DSN)
+		if err != nil {
+			log.Fatalf("Не удалось инициализировать PostgreSQL: %v", err)
+		}
+		defer store.Close()
+	case "memory":
+		store = memory.New()
+	default:
+		log.Fatalf("Неизвестный тип хранилища: %s", *storageType)
+	}
+
+	srv := server.New(cfg, store)
+	if err := srv.Run(); err != nil {
+		log.Fatalf("Не удалось запустить сервер: %v", err)
+	}
+}
