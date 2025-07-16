d7b74c257e8f8b24e4c983d76367cbe7d2305161
Butyrin Ivan <vanya.1.butyrin@gmail.com>
2025-07-16 22:11:56 +0300

    server update

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,15 +1,21 @@
 package server
 
 import (
+	"context"
+	"errors"
 	"net/http"
+	"strings"
+	"time"
 
+	"github.com/99designs/gqlgen/graphql"
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/ButyrinIA/system/internal/config"
-	"github.com/ButyrinIA/system/internal/graphql"
+	mygraphql "github.com/ButyrinIA/system/internal/graphql"
 	"github.com/ButyrinIA/system/internal/storage"
 	"github.com/gorilla/websocket"
+	"github.com/vektah/gqlparser/v2/gqlerror"
 )
 
 type Server struct {
@@ -22,21 +28,58 @@ func New(cfg *config.Config, storage storage.Storage) *Server {
 }
 
 func (s *Server) Run() error {
-	srv := handler.NewDefaultServer(graphql.NewExecutableSchema(graphql.Config{
-		Resolvers: &graphql.Resolver{
-			Storage:             s.storage,
-			SubscriptionHandler: graphql.NewSubscriptionHandler(),
-		},
-	}))
+	resolver := mygraphql.NewResolver(s.storage)
+	executableSchema := mygraphql.NewExecutableSchema(mygraphql.Config{
+		Resolvers: resolver,
+	})
+	srv := handler.NewDefaultServer(executableSchema)
 
-	srv.AddTransport(transport.Websocket{
+	// Configure WebSocket transport
+	srv.AddTransport(&transport.Websocket{
 		Upgrader: websocket.Upgrader{
-			CheckOrigin: func(r *http.Request) bool { return true },
+			CheckOrigin: func(r *http.Request) bool {
+				return true
+			},
 		},
+		KeepAlivePingInterval: 10 * time.Second,
 	})
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	// Authentication middleware
+	srv.AroundOperations(func(ctx context.Context, next graphql.OperationHandler) graphql.ResponseHandler {
+		oc := graphql.GetOperationContext(ctx)
+		if oc == nil {
+			return next(ctx)
+		}
+
+		authHeader := oc.Headers.Get("Authorization")
+		if authHeader != "" {
+			if !strings.HasPrefix(authHeader, "Bearer ") {
+				oc.Error(ctx, gqlerror.Errorf("Invalid authorization header format"))
+				return next(ctx) // Продолжаем выполнение, ошибка уже добавлена
+			}
+
+			token := strings.TrimPrefix(authHeader, "Bearer ")
+			userID, err := validateJWT(token)
+			if err != nil {
+				oc.Error(ctx, gqlerror.Errorf("Invalid token: %v", err))
+				return next(ctx)
+			}
+
+			ctx = context.WithValue(ctx, "userID", userID)
+		}
+
+		return next(ctx)
+	})
+
+	http.Handle("/", playground.Handler("GraphQL Playground", "/query"))
 	http.Handle("/query", srv)
 
 	return http.ListenAndServe(":"+s.cfg.Server.Port, nil)
+}
+
+func validateJWT(token string) (string, error) {
+	if token == "" {
+		return "", errors.New("empty token")
+	}
+	return "user1", nil
 }
